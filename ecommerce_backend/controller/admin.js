cfd332bf7148a1d636c33f94d4f432e47fc40805
irfanpm <muhammmedirfanpm@gmail.com>
2023-09-12 12:11:29 +0530

    just  commit

--- ecommerce_backend/controller/admin.js
+++ ecommerce_backend/controller/admin.js
@@ -3,14 +3,12 @@ var jwt = require("jsonwebtoken");
 var productSchema = require("../MODELS/productdb");
 const { auth_productschema } = require("./validation_schema");
 
-
 module.exports = {
-
   login: async (req, res) => {
     try {
       const admin = {
         username: "admin",
-        password: "admin"
+        password: "admin",
       };
       const { Username, Password } = req.body;
 
@@ -18,22 +16,21 @@ module.exports = {
         let resp = {
           id: admin.username,
         };
-        let token = jwt.sign(resp,process.env.ACESS_ADMINTOKEN_SECRET, { expiresIn: 86400 });
-        res
-          .status(200)
-          .json({
-            status: "success",
-            message: "successfullt logged in",
-            data: { jwt_token: token },
-          });
+        let token = jwt.sign(resp, process.env.ACESS_ADMINTOKEN_SECRET, {
+          expiresIn: 86400,
+        });
+        res.status(200).json({
+          status: "success",
+          message: "successfullt logged in",
+          data: { jwt_token: token },
+        });
       }
     } catch (error) {
       res.send(error);
     }
   },
 
-  
-  users: async (req, res) => {
+  getAllUsers: async (req, res) => {
     const users = await userSchema.find();
 
     res.status(200).json({
@@ -42,7 +39,7 @@ module.exports = {
       data: users,
     });
   },
-  user: async (req, res) => {
+  getUserById: async (req, res) => {
     const user = await userSchema.find({ _id: req.params.id });
     res.status(200).json({
       status: "success",
@@ -50,7 +47,7 @@ module.exports = {
       data: user,
     });
   },
-  products: async (req, res) => {
+  getAllProducts: async (req, res) => {
     const products = await productSchema.find();
 
     res.status(200).json({
@@ -59,11 +56,11 @@ module.exports = {
       data: products,
     });
   },
-  categoryproduct: async (req, res) => {
+  getCategoryProducts: async (req, res) => {
     const categoryprd = await productSchema.find({
       category: req.params.categoryname,
     });
-    
+
     if (categoryprd.length != 0) {
       res.status(200).json({
         status: "success",
@@ -74,7 +71,7 @@ module.exports = {
       res.json("this category not avilable");
     }
   },
-  product: async (req, res) => {
+  getProductById: async (req, res) => {
     const product = await productSchema.find({ _id: req.params.id });
     if (product.length != 0) {
       res.status(200).json({
@@ -86,10 +83,9 @@ module.exports = {
       res.json("this id product not available");
     }
   },
-  addproduct: async (req, res) => {
-    const {error,value}=await auth_productschema.validate(req.body)
+  addProduct: async (req, res) => {
+    const { error, value } = await auth_productschema.validate(req.body);
     const { title, description, image, price, category } = value;
-
 
     if (error) {
       res.status(422).json({
@@ -97,122 +93,95 @@ module.exports = {
         message: error.details[0].message,
       });
     } else {
-
-
-    await productSchema.create({
-      title: title,
-      description: description,
-      price: price,
-      image: image,
-      category: category,
-    });
-    res.status(201).json({
-      status: "success",
-      message: "Successfully created a product.",
-    });
-  }
+      await productSchema.create({
+        title: title,
+        description: description,
+        price: price,
+        image: image,
+        category: category,
+      });
+      res.status(201).json({
+        status: "success",
+        message: "Successfully created a product.",
+      });
+    }
   },
-  updateproduct: async (req, res) => {
-    const {error,value}=await auth_productschema.validate(req.body) 
+  updateProduct: async (req, res) => {
+    const { error, value } = await auth_productschema.validate(req.body);
     const { id, title, description, image, price, category } = value;
-
+    
 
     if (error) {
       res.status(422).json({
         status: "error",
         message: error.details[0].message,
       });
-    }else {
-    await productSchema.findByIdAndUpdate(id, {
-      $set: {
-        title: title,
-        description: description,
-        image: image,
-        price: price,
-        category: category,
-      },
-    });
+    } else {
+      await productSchema.findByIdAndUpdate(id, {
+        $set: {
+          title: title,
+          description: description,
+          image: image,
+          price: price,
+          category: category,
+        },
+      });
 
-    res.json({
-      status: "success",
-      message: "Successfully updated a product.",
-    });
-  }
-  },
-  deleteproduct:async(req,res)=>{
-    const {id}=req.body
-    const deleteprd=await productSchema.find({_id:id})
-    if(deleteprd.length!=0){
-        await productSchema.deleteOne({_id:id})
-        res.json({
-            status: 'success',
-            message: 'Successfully deleted a product.',
-            })
-
+      res.json({
+        status: "success",
+        message: "Successfully updated a product.",
+      });
     }
-    else{
-        res.json("this id product not available")
-    }
-
-
-    
-
-
   },
-  status:async(req,res)=>{
-    const aggregation=userSchema.aggregate([
+  deleteProduct: async (req, res) => {
+    const { id } = req.body;
+    const deleteProduct = await productSchema.find({ _id: id });
+    if (deleteProduct.length != 0) {
+      await productSchema.deleteOne({ _id: id });
+      res.json({
+        status: "success",
+        message: "Successfully deleted a product.",
+      });
+    } else {
+      res.json("this id product not available");
+    }
+  },
+  getStatus: async (req, res) => {
+    const aggregation = userSchema.aggregate([
       {
-        $unwind:'$order'
+        $unwind: "$order",
       },
       {
-        $group:{
-          _id:null,
-          totalRevenue: { $sum: '$order.totalamount' },
-          totalItemsSold: { $sum: { $size: '$order.product' }}
-
-        }
-      }
-    ])
+        $group: {
+          _id: null,
+          totalRevenue: { $sum: "$order.totalamount" },
+          totalItemsSold: { $sum: { $size: "$order.product" } },
+        },
+      },
+    ]);
     const result = await aggregation.exec();
     const totalRevenue = result[0].totalRevenue;
     const totalItemsSold = result[0].totalItemsSold;
 
-
-
-
     res.json({
-      status: 'success',
-      message: 'Successfully fetched stats.',
-      data:{
-        'TotalRevenue': totalRevenue,
-        'Total Items Sold':totalItemsSold
-
-      }
-      })
-
+      status: "success",
+      message: "Successfully fetched stats.",
+      data: {
+        TotalRevenue: totalRevenue,
+        "Total Items Sold": totalItemsSold,
+      },
+    });
   },
-  orderdetails:async(req,res)=>{
-
-
-    const order =await userSchema.find({},"order")
-    orderDetails = order.filter((item)=>{
-     return item.order.length>0
-    })
-
+  getOrderDetails: async (req, res) => {
+    const order = await userSchema.find({}, "order");
+    orderDetails = order.filter((item) => {
+      return item.order.length > 0;
+    });
 
     res.json({
-      status: 'success',
-      message: 'Successfully fetched order detail.',
-      data: orderDetails
-      })
-
-  
-
-
-
-
-  }
-
-
-
+      status: "success",
+      message: "Successfully fetched order detail.",
+      data: orderDetails,
+    });
+  },
 };
--- ecommerce_backend/controller/user.js
+++ ecommerce_backend/controller/user.js
@@ -2,8 +2,9 @@ var userSchema = require("../MODELS/userdb");
 var jwt = require("jsonwebtoken");
 var productSchema = require("../MODELS/productdb");
 require("dotenv").config();
+const bcrypt=require('bcrypt')
 const { authschema } = require("./validation_schema");
-let temp
+let temp;
 module.exports = {
   register: async (req, res) => {
     const { error, value } = await authschema.validate(req.body);
@@ -15,66 +16,76 @@ module.exports = {
         message: error.details[0].message,
       });
     } else {
+      bcrypt.hash(password,10,async function(err,hash){
       await userSchema.create({
         Username: username,
         Email: email,
-        Password: password,
-
-      });
+        Password: hash,
+      })
+    });
       res.status(200).json({
         status: "success",
         message: "successfully register ",
-      })
+      });
     }
-
-  
   },
 
   login: async (req, res) => {
-    const {error,value}=await authschema.validate(req.body)
+    const { error, value } = await authschema.validate(req.body);
     const { username, password } = value;
 
-    
     if (error) {
       res.status(422).json({
         status: "error",
         message: error.details[0].message,
       });
-    }else{
-
-    const login = await userSchema.find({
-      Username: username,
-      Password: password,
-    });
-
-    if (login.length != 0) {
-      let resp = {
-        id: login[0].id,
-      };
-      let token = jwt.sign(resp, process.env.ACESS_TOKEN_SECRET, { expiresIn: 86400 });
-      res.status(200).json({
-        status: "success",
-        message: "successfully login ",
-        auth: true,
-        token: token
-      })
     } else {
-      res.json("this user not available");
-    }
+      const user = await userSchema.find({
+        Username: username,
+      })
+      console.log(user)
+      console.log(password)
+
+      if (user) {
+        bcrypt.compare(password,user[0].Password,(err,result)=>{
+          if(result){
+        let resp = {
+          id: user[0].id,
+        };
+        let token = jwt.sign(resp, process.env.ACESS_TOKEN_SECRET, {
+          expiresIn: 86400,
+        });
+        if(token){
+        res.status(200).json({
+          status: "success",
+          message: "successfully login ",
+          auth: true,
+          token: token,
+        });
+
+      }
+          
+    }else{
+        res.json({err:"failure"})
+      }
+    });
   }
+       else {
+        res.json("this user not available");
+      }
+    }
   },
 
-  products: async (req, res) => {
-    const product= await productSchema.find()
+  viewProducts: async (req, res) => {
+    const product = await productSchema.find();
     res.status(200).json({
       status: "success",
       message: "successfully fetched user product details",
       data: product,
-    })
-
+    });
   },
 
-  productcategory: async (req, res) => {
+  viewProductCategory: async (req, res) => {
     const product = req.params.categoryname;
 
     const prd = await productSchema.find({
@@ -85,12 +96,12 @@ module.exports = {
         status: "success",
         message: "successfully fetched user product details",
         data: prd,
-      })
-      } else {
+      });
+    } else {
       res.json("the product not available");
     }
   },
-  product: async (req, res) => {
+  viewOneProduct: async (req, res) => {
     const product = req.params.id;
 
     const productdetail = await productSchema.find({
@@ -101,12 +112,13 @@ module.exports = {
         status: "success",
         message: "successfully fetched user product details",
         data: productdetail,
-      })
+      });
     } else {
       res.json("this product is not avilable");
     }
   },
-  addcart: async (req, res) => {
+  addToCart: async (req, res) => {
+    //TODO: add updateOne with add to set
     for (x of req.body.product) {
       const avilable = await productSchema.find({ _id: x.id });
       if (avilable.length != 0) {
@@ -117,26 +129,26 @@ module.exports = {
         res.status(200).json({
           status: "success",
           message: "successfully add cart",
-        })     
-       } else {
+        });
+      } else {
         res.send("this product not avilable");
       }
     }
   },
 
-  cartview: async (req, res) => {
+  viewCart: async (req, res) => {// TODO: standarise err responce
     const user = await userSchema.find({ _id: req.params.id }).populate("cart");
     if (user[0].cart.length != 0) {
       res.status(200).json({
         status: "success",
         message: "successfully fetched user cart",
-        data: user[0].cart
-      })   
-     } else {
+        data: user[0].cart,
+      });
+    } else {
       res.json(" no product available");
     }
   },
-  addwishlist: async (req, res) => {
+  addWishlist: async (req, res) => {
     const prd = req.body.product;
     for (x in prd) {
       if (prd.length != 0) {
@@ -144,28 +156,28 @@ module.exports = {
           { _id: req.params.id },
           { $push: { wishlist: prd[x] } }
         );
-      res.status(200).json({
-        status: "success",
-        message: "successfully added product in wishlist",
-      })
+        res.status(200).json({
+          status: "success",
+          message: "successfully added product in wishlist",
+        });
       } else {
         res.send("{status:this product not available}");
       }
     }
   },
-  wishlistview: async (req, res) => {
+  viewWishlist: async (req, res) => {
     const user = await userSchema
       .find({ _id: req.params.id })
       .populate("wishlist");
 
-      res.status(200).json({
-        status: "success",
-        message: "successfully fetched user wishlist",
-        data: user[0].wishlist
-      })  
+    res.status(200).json({
+      status: "success",
+      message: "successfully fetched user wishlist",
+      data: user[0].wishlist,
+    });
   },
 
-  wishlistdelete: async (req, res) => {
+  wishlistDelete: async (req, res) => {
     const product = await productSchema.find({ _id: req.params.id });
     if (product.length != 0) {
       await userSchema.updateOne(
@@ -176,21 +188,16 @@ module.exports = {
       res.status(200).json({
         status: "success",
         message: "successfully fetched user cart",
-      })    
-      } else {
+      });
+    } else {
       res.json("this product not avilable");
     }
-
-
   },
-  stripe: async (req, res) => {
+  paymentMethod: async (req, res) => {
     const stripe = require("stripe")(process.env.STRIPE_KEY);
     const user = await userSchema.find({ _id: req.params.id }).populate("cart");
     const cartitem = user[0].cart.map((item) => {
-      
       return {
-       
-        
         price_data: {
           currency: "usd",
           product_data: {
@@ -201,46 +208,52 @@ module.exports = {
         },
         quantity: 1,
       };
-      
     });
     console.log(cartitem);
-    if(cartitem!=0){
-   
+    if (cartitem != 0) {
+      const session = await stripe.checkout.sessions.create({
+        line_items: cartitem,
+        mode: "payment",
+        success_url: `http://127.0.0.1:9000/api/users/payment/success`,
+        cancel_url: `http://127.0.0.1:9000/api/users/payment/cancel`,
+      });
+      temp = {
+        cartitem: user[0].cart,
+        id: req.params.id,
+        paymentid: session.id,
+        amount: session.amount_total / 100,
+      };
 
-    const session = await stripe.checkout.sessions.create({
-      line_items: cartitem,
-      mode: "payment",
-      success_url: `http://127.0.0.1:9000/api/users/payment/success`,
-      cancel_url: `http://127.0.0.1:9000/api/users/payment/cancel`,
-    });
-    temp={
-      cartitem:user[0].cart,
-      id:req.params.id,
-      paymentid:session.id,
-      amount:session.amount_total/100
-
-     }
-
-    res.send({ url: session.url });
-    }else{
-      res.send("user no cart item")
+      res.send({ url: session.url });
+    } else {
+      res.send("user no cart item");
     }
-    
   },
-  sucess: async (req, res) => {
-    const user = await userSchema.find({_id:temp.id})
-     if(user.length!=0){
-        await userSchema.updateOne({_id:temp.id},{$push:{order:{product:temp.cartitem,date:new Date(),orderid:Math.random(),paymentid:temp.paymentid,totalamount:temp.amount   }}})
-        await userSchema.updateOne({_id:temp.id},{cart:[]})
-
-     }
-     res.status(200).json({
+  paymentSuccess: async (req, res) => {
+    const user = await userSchema.find({ _id: temp.id });
+    if (user.length != 0) {
+      await userSchema.updateOne(
+        { _id: temp.id },
+        {
+          $push: {
+            order: {
+              product: temp.cartitem,
+              date: new Date(),
+              orderid: Math.random(),
+              paymentid: temp.paymentid,
+              totalamount: temp.amount,
+            },
+          },
+        }
+      );
+      await userSchema.updateOne({ _id: temp.id }, { cart: [] });
+    }
+    res.status(200).json({
       status: "success",
       message: "successfully added in order",
-    })   
-   },
-  cancel: async (req, res) => {
+    });
+  },
+  paymentCancel: async (req, res) => {
     res.json("cancel");
   },
-
 };
